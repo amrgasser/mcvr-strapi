a5943460818045d7fd8be3599a83ada889cc468b
amrgasser <amr_gasser@hotmail.com>
2024-04-03 17:08:29 +0200

    fix and integration

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,24 +1,24 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'aws-s3',
+    provider: "aws-s3",
     providerOptions: {
       s3Options: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
         params: {
-          Bucket: env('AWS_BUCKET_NAME'),
+          Bucket: env("AWS_BUCKET_NAME"),
         },
-      }
+      },
     },
     // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
     actionOptions: {
       upload: {
-        ACL: null
+        ACL: null,
       },
       uploadStream: {
-        ACL: null
+        ACL: null,
       },
-    }
-  }
+    },
+  },
 });
